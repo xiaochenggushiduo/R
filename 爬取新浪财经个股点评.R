22eafbc6a97b77bb34a83d24456f812d1c25966b
xiaochenggushiduo <wodecuoabc@sina.com>
2018-06-11 14:23:31 +0800

    增加翻页

--- 爬取新浪财经个股点评.R
+++ 爬取新浪财经个股点评.R
@@ -1,7 +1,12 @@
 library('rvest')
 library(xml2)
-url <- 'http://finance.sina.com.cn/stock/'
-webpage<-read_html(url,encoding = 'utf-8')#在源码中搜索“charset”查找该网页编码方式
+j<-1:5
+for (i in j) {
+  u<-paste("http://roll.finance.sina.com.cn/finance/zq1/gsjsy/index_",i,".shtml")
+  url <- u
+}
+
+webpage<-read_html(url,encoding = 'gb2312')#在源码中搜索“charset”查找该网页编码方式
 a_data_html<-html_nodes(webpage,'p')
 a_data<-html_text(a_data_html)
 head(a_data)
@@ -15,4 +20,5 @@ mixseg <- worker()
 a<-segment(text, mixseg)
 a<- table(a)
 wordcloud2(a,shape = 'star')
-
+png(file="/Users/song/Desktop/大盘词云分析.png", bg="transparent")
+dev.off()
