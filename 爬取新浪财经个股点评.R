e17fe18717ac55a6b0f2d4a353e34fc037ae325e
xiaochenggushiduo <wodecuoabc@sina.com>
2018-06-21 13:58:07 +0800

    遍历五页

--- 爬取新浪财经个股点评.R
+++ 爬取新浪财经个股点评.R
@@ -1,24 +1,22 @@
 library('rvest')
 library(xml2)
-j<-1:5
-for (i in j) {
-  u<-paste("http://roll.finance.sina.com.cn/finance/zq1/gsjsy/index_",i,".shtml")
+#j<-1:5
+#for (i in j) {
+  u<-paste("http://stock.10jqka.com.cn/thsgd/")
   url <- u
-}
-
+#}
 webpage<-read_html(url,encoding = 'gb2312')#在源码中搜索“charset”查找该网页编码方式
-a_data_html<-html_nodes(webpage,'p')
+a_data_html<-html_nodes(webpage,'a')
 a_data<-html_text(a_data_html)
 head(a_data)
-write.csv(a_data,file="/Users/song/Desktop/股票新闻.csv")
+news<-write.csv(a_data,file=paste("/Users/song/Desktop/news.csv"))
 
 #分词
 library(jiebaR,jiebaRD)
 library(wordcloud2)
-text<-as.character(news)
-mixseg <- worker() 
+text<-news$x
+mixseg <- worker("mix") 
 a<-segment(text, mixseg)
 a<- table(a)
 wordcloud2(a,shape = 'star')
-png(file="/Users/song/Desktop/大盘词云分析.png", bg="transparent")
-dev.off()
+
