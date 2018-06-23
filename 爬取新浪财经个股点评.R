67ee3e7d786d7242f354b1a232bf9ae7b13f083f
xiaochenggushiduo <wodecuoabc@sina.com>
2018-06-23 18:58:29 +0800

    ？只保存第五页的内容

--- 爬取新浪财经个股点评.R
+++ 爬取新浪财经个股点评.R
@@ -1,15 +1,15 @@
 library('rvest')
 library(xml2)
-#j<-1:5
-#for (i in j) {
-  u<-paste("http://stock.10jqka.com.cn/thsgd/")
-  url <- u
-#}
+for (i in 1:5) {
+u<-paste("http://roll.finance.sina.com.cn/finance/zq1/gsjsy/index_",i,".shtml")
+url <- u
 webpage<-read_html(url,encoding = 'gb2312')#在源码中搜索“charset”查找该网页编码方式
 a_data_html<-html_nodes(webpage,'a')
 a_data<-html_text(a_data_html)
 head(a_data)
 news<-write.csv(a_data,file=paste("/Users/song/Desktop/news.csv"))
+}
+
 
 #分词
 library(jiebaR,jiebaRD)
